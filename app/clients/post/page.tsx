b1dde9c1bffe1bb66d1c7b513675017c1472e80e
Minh Nhat <nhokkudo143@gmail.com>
2025-12-08 23:31:18 +0700

    setup next and login page

---
+++ app/clients/post/page.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Post: React.FC = () => {
+
+    return(
+        <>
+            <div>
+                <h1>Post</h1>
+            </div>
+        </>
+    )
+}
+export default Post
