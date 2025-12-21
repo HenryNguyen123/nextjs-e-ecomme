b685c1e1ccfe2dc9d1bd05bee0d7361537dde70e
Minh Nhat <nhokkudo143@gmail.com>
2025-12-21 18:47:39 +0700

    check responsive

--- app/components/home/model/Model.component.tsx
+++ app/components/home/model/Model.component.tsx
@@ -1,19 +1,18 @@
-const ModleComponent:React.FC = () => {
-    
-    return(
-        <>
-            <div className="w-full">
-                <div className="flex justify-center">
-                    <div className="lg:max-w-7xl ">
-                        <div className="items-center">
-                            <h1 className="text-5xl w-2xl">
-                                <strong className="flex items-center"> Essential Integrations with Modern Design</strong>
-                            </h1>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
-export default ModleComponent
+const ModleComponent: React.FC = () => {
+  return (
+    <div className="w-full">
+      <div className="flex justify-center my-36 px-4">
+        <div className="max-w-2xl text-center">
+          <h1 className="text-4xl md:text-5xl font-bold leading-tight">
+            Essential Integrations with Modern Design
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor eros. Donec vitae tortor lacus. Phasellus aliquam ante in maximus.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModleComponent;
